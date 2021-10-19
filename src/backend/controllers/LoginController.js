e84a4910ba2a9a49e3987d5af86971642a790d40
hydroxa <alexgillott489@gmail.com>
2021-10-19 13:52:32 +0100

    Rebasing

--- src/backend/controllers/LoginController.js
+++ src/backend/controllers/LoginController.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const router = express.router();
 
-router.get("/login", async (req,res) => {
+router.get("/login", async (req, res) => {
     const username = req.query.username;
 	const password = req.query.password;
 });
