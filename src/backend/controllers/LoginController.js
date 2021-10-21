30fe295dd1dad249c3b5adc2081002769a73293d
hydroxa <alexgillott489@gmail.com>
2021-10-21 16:44:47 +0100

    Removed codes and get

--- src/backend/controllers/LoginController.js
+++ src/backend/controllers/LoginController.js
@@ -5,17 +5,11 @@ import { secret } from "../config";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-    res.send(
-        'Login page. Please use POST. <a href="https://github.com/Hydroxa/mars-rover-js/wiki/login">Check the wiki here</a>'
-    );
-});
 router.post("/", async (req, res) => {
     const body = req.body;
     if (body === undefined || body.username === undefined || body.password === undefined) {
         res.status(400).send({
             message: "Malformed request. A required object was undefined",
-            code: 2,
         });
     }
     const isValid = await UserService.isUserValid(body.username, body.password);
@@ -23,12 +17,10 @@ router.post("/", async (req, res) => {
         res.send({
             message: `Valid credentials. Welcome, ${body.username}`,
             token: createTokenForUser(body.username),
-            code: 0,
         });
     } else {
         res.status(401).send({
             message: "Invalid credentials :(",
-            code: 1,
         });
     }
 });
