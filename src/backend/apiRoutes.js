6f3ec398b7a4d00943f71353a554fa613d6cc3a9
JabrilMahamud <91251262+JabrilMahamud@users.noreply.github.com>
2021-10-21 14:02:51 +0100

    pre-auth changes

--- src/backend/apiRoutes.js
+++ src/backend/apiRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import QuotationsController from "./controllers/QuotationsController.js";
-
+import PhotosController from "./controllers/PhotosController"
 const router = express.Router();
 
 router.use("/quotations", QuotationsController);
+router.use("/photos",PhotosController)
 
 router.use((req, res) => {
     res.sendStatus(404);
---
+++ src/backend/controllers/PhotosController.js
@@ -0,0 +1,34 @@
+import express from "express";
+
+const router = express.Router();
+
+function ApiData(){
+    router.get("/photos", function (req, res) {
+        got
+          .get(
+            "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY"
+          )
+          .json()
+          .then(function (body) {
+            let names = [];
+            let ApiKey = "";
+            for (let a =0;a>body.photos;a++) {
+                res.send(body.photos[a])
+            }
+            return got
+              .get(
+            "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY"
+              )
+              .json();
+          })
+          .then(function (response) {
+            res.send(body.photos[0]);
+          })
+          .catch(function (ERR) {
+            console.error(ERR);
+            res.send(ERR)
+          });
+      });
+}
+
+export default ApiData
