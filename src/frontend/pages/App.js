5ba1229bd34d75b9ed81804284f57f7a5ddd61e6
JabrilMahamud <91251262+JabrilMahamud@users.noreply.github.com>
2021-10-21 11:40:17 +0100

    Blank photo browsing page

--- src/frontend/pages/App.js
+++ src/frontend/pages/App.js
@@ -3,11 +3,15 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./home/Home";
 import "./cssreset.css";
 import "./App.css";
+import Photos from "./Photos/Photos";
+
+
 
 export default function App() {
     return (
         <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/photos" component={Photos} />
         </Switch>
     );
 }
