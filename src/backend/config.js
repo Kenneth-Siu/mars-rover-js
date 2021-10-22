a92fd706d22f3439394dd7d81924c05a6cf72125
JabrilMahamud <91251262+JabrilMahamud@users.noreply.github.com>
2021-10-22 10:53:30 +0100

    removed PhotoControllers and updated packages as well as config

--- src/backend/config.js
+++ src/backend/config.js
@@ -1,2 +1,2 @@
 export const connectionString = "postgres://username:password@localhost:5432/mars_rover";
-export const ApiKey = "EoVR4QPXwBlAUssFZTbLHQ7GKV4SkG6MetuF42nH"
+export const ApiKey = "";
--- src/frontend/api/PhotosApi.js
+++
@@ -1,7 +0,0 @@
-import { get } from "./Api.js";
-
-const baseUrl = "/photos";
-
-export async function getQuotationOfTheDay() {
-    return await get(baseUrl);
-}
