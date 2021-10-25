46e2ffc7e66bc6a9eae19a94b3cbe31d6590e89f
Kit Howlett <kit.howlett@gmail.com>
2021-10-25 11:28:30 +0100

    User can now login using login credentials.

--- src/frontend/pages/login/Login.js
+++ src/frontend/pages/login/Login.js
@@ -1,21 +1,46 @@
 import { useState } from "react";
 import LoginForm from "../components/LoginForm";
+import { setToken, getToken } from '../../../backend/jwtToken.js';
 
-const Login = () => {
+const Login = (props) => {
     const [user, setUser] = useState({ username: "", password: "" });
     const [error, setError] = useState("");
 
     const handleLogin = (details) => {
+        const loginDetails = {
+            username: details.username,
+            password: details.password,
+        };
 
-        console.log(`Username is: ${details.username}`);
-        console.log(`Password is: ${details.password}`);
+        fetch("/api/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(loginDetails),
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    response.json().then((json) => {
+                        setToken(json.token);
+                        props.history.push("/");
+                    });
+                } else if (response.status === 401) {
+                    throw new Error('Incorrect login details');
+                } else {
+                    throw new Error('Could not login');
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     };
-    
+
     return (
         <>
             <title>Mars Rover App - Login</title>
             <main className="login-page page-container">
-                <LoginForm login={handleLogin} error={error}/>
+                <LoginForm login={handleLogin} error={error} />
             </main>
         </>
     );
