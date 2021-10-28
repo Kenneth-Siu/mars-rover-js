4cbce954af8ef5cd9b5738e4b59a64f4eb9c2056
JabrilMahamud <91251262+JabrilMahamud@users.noreply.github.com>
2021-10-28 14:16:03 +0100

    2nd try completing kenny's requests,

--- src/backend/apiRoutes.js
+++ src/backend/apiRoutes.js
@@ -2,11 +2,12 @@ import express from "express";
 import QuotationsController from "./controllers/QuotationsController.js";
 import LoginController from "./controllers/LoginController.js";
 
+const router = express.Router();
 
 router.use("/quotations", QuotationsController);
 router.use("/login", LoginController);
 router.use((_, res) => {
-    res.sendStatus(404);
+  res.sendStatus(404);
 });
 
 export default router;
