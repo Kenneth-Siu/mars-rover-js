3a066897377d22ab6bc69b71f9ef99c1cc3ba079
Kit Howlett <kit.howlett@gmail.com>
2021-10-28 15:10:27 +0100

    Fix merge conficts

--- src/frontend/pages/home/Home.js
+++ src/frontend/pages/home/Home.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import * as QuotationsApi from "../../api/QuotationsApi";
 import Header from '../components/Header';
 import "./Home.css";
-import NavigationBar from "../../Components/NavigationBar.js";
 
 export default function Home() {
     const [quotation, setQuotation] = useState();
