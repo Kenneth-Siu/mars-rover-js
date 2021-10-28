0be2a25e5cc3426e98ef0e4d16210628f3115b0c
JabrilMahamud <91251262+JabrilMahamud@users.noreply.github.com>
2021-10-28 12:16:37 +0100

    updated accouring to kenny's comments

--- src/backend/apiRoutes.js
+++ src/backend/apiRoutes.js
@@ -1,10 +1,5 @@
 import express from "express";
 import QuotationsController from "./controllers/QuotationsController.js";
-import PhotosController from "./controllers/PhotosController"
-const router = express.Router();
-
-router.use("/quotations", QuotationsController);
-router.use("/photos",PhotosController)
 import LoginController from "./controllers/LoginController.js";
 
 
